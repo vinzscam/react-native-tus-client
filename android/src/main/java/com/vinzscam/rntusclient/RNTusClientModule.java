c9bcf324d53179480e2508d3dc66143b1e61d165
Juliano Duarte <juliano@digitalrelab.com>
2019-10-09 14:51:05 -0400

    fix: use TusAndroidUpload instead of TusUpload
    TusAndroidUpload automatically parses "content://" uris

--- android/src/main/java/com/vinzscam/rntusclient/RNTusClientModule.java
+++ android/src/main/java/com/vinzscam/rntusclient/RNTusClientModule.java
@@ -2,6 +2,7 @@
 package com.vinzscam.rntusclient;
 
 import android.content.SharedPreferences;
+import android.net.Uri;
 
 import com.facebook.react.bridge.Arguments;
 import com.facebook.react.bridge.ReactApplicationContext;
@@ -28,10 +29,10 @@ import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.atomic.AtomicBoolean;
 
 import io.tus.android.client.TusPreferencesURLStore;
+import io.tus.android.client.TusAndroidUpload;
 import io.tus.java.client.ProtocolException;
 import io.tus.java.client.TusClient;
 import io.tus.java.client.TusExecutor;
-import io.tus.java.client.TusUpload;
 import io.tus.java.client.TusUploader;
 
 public class RNTusClientModule extends ReactContextBaseJavaModule {
@@ -63,31 +64,28 @@ public class RNTusClientModule extends ReactContextBaseJavaModule {
     Map<String, Object> rawMetadata = options.getMap("metadata").toHashMap();
 
     Map<String, String> metadata = new HashMap<>();
-    for(String key: rawMetadata.keySet()) {
+    for (String key : rawMetadata.keySet()) {
       metadata.put(key, String.valueOf(rawMetadata.get(key)));
     }
     Map<String, String> headers = new HashMap<>();
-    for(String key: rawHeaders.keySet()) {
+    for (String key : rawHeaders.keySet()) {
       headers.put(key, String.valueOf(rawHeaders.get(key)));
     }
 
-    File file = new File(fileUrl);
-    TusUpload upload = null;
     try {
-      upload = new TusUpload(file);
       String uploadId = UUID.randomUUID().toString();
       TusRunnable executor = new TusRunnable(fileUrl, uploadId, endpoint, metadata, headers);
       this.executorsMap.put(uploadId, executor);
       callback.invoke(uploadId);
-    } catch(FileNotFoundException | MalformedURLException e) {
-      callback.invoke((Object)null, e.getMessage());
+    } catch (FileNotFoundException | MalformedURLException e) {
+      callback.invoke((Object) null, e.getMessage());
     }
   }
 
   @ReactMethod
   public void resume(String uploadId, Callback callback) {
     TusRunnable executor = this.executorsMap.get(uploadId);
-    if(executor != null) {
+    if (executor != null) {
       pool.submit(executor);
       callback.invoke(true);
     } else {
@@ -99,46 +97,45 @@ public class RNTusClientModule extends ReactContextBaseJavaModule {
   public void abort(String uploadId, Callback callback) {
     try {
       TusRunnable executor = this.executorsMap.get(uploadId);
-      if(executor != null) {
+      if (executor != null) {
         executor.finish();
       }
-      callback.invoke((Object)null);
-    } catch(IOException | ProtocolException e) {
+      callback.invoke((Object) null);
+    } catch (IOException | ProtocolException e) {
       callback.invoke(e);
     }
   }
 
   class TusRunnable extends TusExecutor implements Runnable {
-    private TusUpload upload;
+    private TusAndroidUpload upload;
     private TusUploader uploader;
     private String uploadId;
     private TusClient client;
     private boolean shouldFinish;
     private boolean isRunning;
 
-    public TusRunnable(String fileUrl,
-                       String uploadId,
-                       String endpoint,
-                       Map<String, String> metadata,
-                       Map<String, String>headers
-    ) throws FileNotFoundException, MalformedURLException {
+    public TusRunnable(String fileUrl, String uploadId, String endpoint, Map<String, String> metadata,
+        Map<String, String> headers) throws FileNotFoundException, MalformedURLException {
       this.uploadId = uploadId;
+
       client = new TusClient();
       client.setUploadCreationURL(new URL(endpoint));
 
-      SharedPreferences pref = getReactApplicationContext().getSharedPreferences("tus", 0);
+      SharedPreferences pref = reactContext.getSharedPreferences("tus", 0);
+
       client.enableResuming(new TusPreferencesURLStore(pref));
       client.setHeaders(headers);
-      File file = new File(fileUrl);
-      upload = new TusUpload((file));
+
+      upload = new TusAndroidUpload(Uri.parse(fileUrl), reactContext);
       upload.setMetadata(metadata);
+
       shouldFinish = false;
       isRunning = false;
     }
 
     protected void makeAttempt() throws ProtocolException, IOException {
       uploader = client.resumeOrCreateUpload(upload);
-      uploader.setChunkSize(1024);
+      uploader.setChunkSize(1024 * 10);
       uploader.setRequestPayloadSize(10 * 1024 * 1024);
 
       do {
@@ -148,19 +145,17 @@ public class RNTusClientModule extends ReactContextBaseJavaModule {
         params.putString("uploadId", uploadId);
         params.putDouble("bytesWritten", bytesUploaded);
         params.putDouble("bytesTotal", totalBytes);
-        reactContext.getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class)
-                .emit(ON_PROGRESS, params);
-      }while(uploader.uploadChunk() > -1 && !shouldFinish);
+        reactContext.getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class).emit(ON_PROGRESS, params);
+      } while (uploader.uploadChunk() > -1 && !shouldFinish);
 
       uploader.finish();
     }
 
     public void finish() throws ProtocolException, IOException {
-      if(isRunning) {
+      if (isRunning) {
         shouldFinish = true;
-      }
-      else {
-        if(uploader!= null){
+      } else {
+        if (uploader != null) {
           uploader.finish();
         }
       }
@@ -176,14 +171,12 @@ public class RNTusClientModule extends ReactContextBaseJavaModule {
         WritableMap params = Arguments.createMap();
         params.putString("uploadId", uploadId);
         params.putString("uploadUrl", uploadUrl);
-        reactContext.getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class)
-                .emit(ON_SUCCESS, params);
+        reactContext.getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class).emit(ON_SUCCESS, params);
       } catch (ProtocolException | IOException e) {
         WritableMap params = Arguments.createMap();
         params.putString("uploadId", uploadId);
         params.putString("error", e.toString());
-        reactContext.getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class)
-                .emit(ON_ERROR, params);
+        reactContext.getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class).emit(ON_ERROR, params);
       }
       isRunning = false;
     }
